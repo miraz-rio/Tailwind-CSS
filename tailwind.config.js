3519a0b08541969b30d94d30d43a32f7ad3c9d83
miraz <mirazbd99@gmail.com>
2024-09-29 23:28:28 +0600

    Dark mode variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports =  { content: ['./src/**/*.{html,js,jsx,ts,tsx}', './index.html', './public/index.html'], theme: { extend: {} }, plugins: [] }
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}',
+      './index.html',
+      './public/index.html',
+    ],
+       darkMode: "media",
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+ 
