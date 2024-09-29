ee6a649eb9e3693f956fc7d2e6b93a53518b764c
miraz <mirazbd99@gmail.com>
2024-09-29 20:41:40 +0600

    tailwing css card design

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+module.exports =  { content: ['./src/**/*.{html,js,jsx,ts,tsx}', './index.html', './public/index.html'], theme: { extend: {} }, plugins: [] }
