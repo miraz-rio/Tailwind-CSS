13627d9b37a58175517484c712ffb27861be3353
miraz <mirazbd99@gmail.com>
2024-09-30 11:54:59 +0600

    Tailwind Configurations - Advanced Concepts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+// TypeScript type hints for better editor support when working with Tailwind CSS.
+
 module.exports = {
-    content: [
-      './src/**/*.{html,js,jsx,ts,tsx}',
-      './index.html',
-      './public/index.html',
-    ],
-       darkMode: "media",
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
- 
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", 
+    "./index.html", 
+    "./public/index.html", 
+  ],
+  darkMode: "media" /* or "class" or "media"*/,
+  theme: {
+    extend: {}, // Extend the default theme here
+  },
+  variants: {
+    extend: {}, // variant settings (like hover, focus, etc.)
+  },
+  plugins: [], // Add plugins if needed
+};
